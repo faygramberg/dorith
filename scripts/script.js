021a01a93e8508f2bd5808780f1e59d5883cbff4
Fay Gramberg <faygramberg@gmail.com>
2016-11-08 19:15:22 +0100

    succesfully included jquery

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,19 @@
 
 $('.grid').masonry({
   itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
+  columnWidth: 200,
   percentPosition: true
+});
+
+
+
+
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  // options...
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
 });
